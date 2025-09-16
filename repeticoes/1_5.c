7ec3347bf50a5f906100240daaab113c7a89c83f
akampfsilva-ai <akampfsilva@gmail.com>
2025-09-16 17:13:18 -0300

    Update 1_5.c

--- repeticoes/1_5.c
+++ repeticoes/1_5.c
@@ -2,28 +2,19 @@
 
 int main ()
 {
-    int n, i=0, f1=1, f2=1, f3=1;
+    int n, i=0, f1=0, f2=0, f3=1;
 
     printf("Digite quantos numeros da sequencia de fibonacci voce deseja: \n");
     scanf(" %d", &n);
 
     while(i<n)
     {
-        if(i==0||i==1)
-        {
-            printf("%d \n", i);
+        printf("%d \n", f1);
+        f1+=f2;
+        f2=f3;
+        f3=f1;
 
-            i++;
-        }
-        else
-        {
-            printf("%d \n", f1);
-            f1+=f2;
-            f2=f3;
-            f3=f1;
-
-            i++;
-        }
+        i++;
     }
 
     return 0;
