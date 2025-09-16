279204f06de252e29ce0b38acdd416c207895edd
akampfsilva-ai <akampfsilva@gmail.com>
2025-09-16 15:56:48 -0300

    Update 2_5.c

---
+++ condicionais/2_5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, y;
+    
+    printf("Digite as coordenadas x ESPACO y: \n");
+    scanf("%d %d", &x, &y);
+
+    if(x>=0&&x<=432&&y>=0&&y<=468)
+        printf("d");
+    else
+        printf("f");
+    
+    return 0;
+}
