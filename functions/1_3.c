07fbc5d3b8c98d63ff5d4580c5f6aa191fa108ea
akampfsilva-ai <akampfsilva@gmail.com>
2025-10-08 12:42:22 +0000

    Modified files

---
+++ functions/1_3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int calculoDigitoVerificador (int num);
+
+int main ()
+{
+    int num, x;
+
+    printf("Digite um número: \n");
+    scanf(" %d \n", &num);
+
+    x=calculoDigitoVerificador(num%1000);
+
+    printf(" %d \n", x);
+
+    return 0;
+}
+
+int calculoDigitoVerificador (int num);
+{
+    num=(num-(num%100))*1+(num%100-((num%100)%10))*2+(num%10)*3;
+
+    return num;
+}
