2c8dc9ed00e2d03ae2e603279df6a124389b4475
unknown <akampfsilva@gmail.com>
2025-10-29 11:11:50 -0300

    Modified file

--- provas/prova2ex1.c
+++ provas/prova2ex1.c
@@ -2,44 +2,52 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define JAJA 49
+#define JAJA 51
+
+void retornaString (char str[])
+{
+    int l, m;
+    int v = strlen(str);
+    char n;
+
+    if (strchr("aeiouAEIOU", str[0]))
+        {
+            for (l=0, m=v-1; l<m; l++, m--)
+                {
+                    n = str[l];
+                    str[l] = str[m];
+                    str[m] = n;
+                }
+        }
+}
 
 int main ()
 {
-    int i, j, k, l, m, n, v;
-    char* matriz [3][3], str[JAJA];
+    int i, j;
+    char matriz [3][3][JAJA];
 
     for (i=0; i<3; i++)
     {
         for (j=0; j<3; j++)
         {
-            printf("Digite a sua string do espaco [%d][%d]:\n", i, j);
+          printf("Digite a string [%d][%d]", i, j);
 
-            fgets(str, JAJA, stdin);
+          fgets(matriz[i][j], JAJA, stdin);
 
-            if(str[0]=='a'||str[0]=='e'||str[0]=='i'||str[0]=='o'||str[0]=='u')
-            {
-                while(str[k] != '\0')
-                {
-                    v=k;
-                }
+          matriz[i][j][strcspn(matriz[i][j], "\n")] = '\0';
 
-                for(l=0;l<v/2;l++)
-                {
-                    for(m=0;m<v;m++)
-                    {
-                        if(l+m==v-1)
-                        {
-                            n = str[l];
-                            str[l] = str[m];
-                            str[m] = n;
-                        }
-                    }
-                }
-            }
-            matriz[i][j]=str;
-            printf("%s", matriz[i][j]);
+          retornaString(matriz[i][j]);
         }
+    }
+
+    printf("Matriz final:\n");
+
+    for (i=0;i<3;i++)
+    {
+        for (j=0;j<3;j++)
+            printf("%s ", matriz[i][j]);
+
+        printf("\n");
     }
 
     return 0;
